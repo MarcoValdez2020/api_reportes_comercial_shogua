54ded845d9b13af3c37a9ac6bcaf14fafc1ce3c0
MARCO ANTONIO VALDEZ RODEA <valdez.rodea.marcoantonio@gmail.com>
2024-12-20 10:51:28 -0600

    Reporte de cierre de mes unode50 listo

--- app/reports/report_routes.py
+++ app/reports/report_routes.py
@@ -62,4 +62,21 @@ async def get_month_end_report_tumi(
     except Exception as e:
         print(f"Error en get_month_end_report_tumi: {e}")
         raise HTTPException(status_code=500, detail="Error interno del servidor")
+
+
+# Ruta expuesta para el reporte de cierre de mes de UNODE50
+@router.get("/get-month-end-report-unode50", response_model=FinallyEndMonthReportTous)
+async def get_month_end_report_unode50(
+    nombre_marca:str, 
+    mes:str
+):
+    try:
+        cierre_mes = report_service.get_month_end_report_unode50(nombre_marca, mes)
+        return cierre_mes
+    
+    except ValueError as e:  # Excepción específica de tu lógica de negocio
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        print(f"Error en get_month_end_report_unode50: {e}")
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
     
--- app/reports/report_service.py
+++ app/reports/report_service.py
@@ -597,9 +597,6 @@ class ReportService:
         ventas_promedio_df = self.calcularVentaPromedio(nombre_marca,fecha_fin_mes_anio_actual)
 
 
-        # reporte_cierre_mes = self.fusionar_dataframes_cierre_mes_ag_y_mu(ventas_mes_anterior_df,ventas_mes_actual_df,
-        #                                                                 ventas_ytd_anio_anterior_df,ventas_ytd_anio_actual,
-        #                                                                 tiendas_df,inventarios_tiendas_df, ventas_promedio_df)
         reporte_cierre_mes = self.fusionar_dataframes_cierre_mes_tumi(ventas_mes_anterior_df,ventas_mes_actual_df,
                                                                         ventas_ytd_anio_anterior_df,ventas_ytd_anio_actual,
                                                                         tiendas_df,inventarios_tiendas_df, ventas_promedio_df,
@@ -609,7 +606,7 @@ class ReportService:
 
         return reporte_cierre_mes
     
-
+    # Funcion para fusionar el reporte de cierre de mes de tumi
     def fusionar_dataframes_cierre_mes_tumi(self, ventas_mes_anterior_df:pd.DataFrame, ventas_mes_actual_df:pd.DataFrame,
                                                 ventas_ytd_anio_anterior_df:pd.DataFrame,ventas_ytd_anio_actual:pd.DataFrame,
                                                 tiendas_df:pd.DataFrame, inventarios_tiendas_df:pd.DataFrame, ventas_promedio_df: pd.DataFrame,
@@ -696,6 +693,243 @@ class ReportService:
 
         #* Realizar la fuision entre los tres dfs, el de tijuana, con cancun y cdmx ya con sus almacenes de cun y cdmx
         ventas_cierre_mes_df = pd.concat([tiendas_sin_cdmx_y_cun, tiendas_cancun_df,tiendas_cdmx_df], ignore_index=True)
+        # Llenamos con 0 los almacenes que no tengan existencia
+        ventas_cierre_mes_df = ventas_cierre_mes_df.fillna(0)
+
+        # Renombramos los campos para coincidir con el modelo de respuesta
+        ventas_cierre_mes_df.rename(columns={'mos':'mos_tienda'}, inplace=True)
+        ventas_cierre_mes_df.rename(columns={'existencia':'existencia_tienda'}, inplace=True)
+
+
+        # print(ventas_cierre_mes_df)
+        # Crear una lista de objetos EndMonthReportAGyMumuso a partir del DataFrame
+        list_of_reports = [
+            StoresEndMonthReportTous(**row.to_dict()) for _, row in ventas_cierre_mes_df.iterrows()
+        ]
+
+        # Formar el totales del reporte final
+        total_venta_mensual_anio_anterior_iva = ventas_cierre_mes_df['venta_mensual_anio_anterior_iva'].sum()
+        total_venta_mensual_anio_actual_iva = ventas_cierre_mes_df['venta_mensual_anio_actual_iva'].sum()
+        total_variacion_mes_porcentaje = ((total_venta_mensual_anio_actual_iva / total_venta_mensual_anio_anterior_iva)-1)*100
+        total_variacion_mes_efectivo = total_venta_mensual_anio_actual_iva - total_venta_mensual_anio_anterior_iva
+
+        total_ytd_anio_anterior_iva = ventas_cierre_mes_df['ytd_anio_anterior_iva'].sum()
+        total_ytd_anio_actual_iva = ventas_cierre_mes_df['ytd_anio_actual_iva'].sum()
+        total_variacion_ytd_porcentaje = ((total_ytd_anio_actual_iva / total_ytd_anio_anterior_iva)-1)*100
+        total_variacion_ytd_efectivo = total_ytd_anio_actual_iva - total_ytd_anio_anterior_iva
+
+        total_existencia_tiendas = float(ventas_cierre_mes_df['existencia_tienda'].sum())
+        total_venta_promedio = float(ventas_cierre_mes_df['venta_promedio'].sum())
+        total_mos_tiendas = total_existencia_tiendas/total_venta_promedio
+
+
+        # Sumamos el total de existencia de bodegas
+        total_existencia_bodegas = float(ventas_cierre_mes_df['existencias_almacen'].sum())
+        total_mos_bodegas = (total_existencia_tiendas + total_existencia_bodegas) / total_venta_promedio
+
+        # Calculamos el mos de los almacenes
+        total_existencia_almacenes = float(suma_stock_almacenes_virtuales)
+        total_mos_almacenes = (total_existencia_tiendas + total_existencia_bodegas + total_existencia_almacenes) / total_venta_promedio
+
+        reporte_final_cierre_mes = FinallyEndMonthReportTous(
+            stores=list_of_reports,
+            existencias_almacen_virtual = total_existencia_almacenes,
+
+            total_venta_mensual_anio_anterior_iva=total_venta_mensual_anio_anterior_iva,
+            total_venta_mensual_anio_actual_iva=total_venta_mensual_anio_actual_iva,
+            total_variacion_mes_porcentaje=total_variacion_mes_porcentaje,
+            total_variacion_mes_efectivo=total_variacion_mes_efectivo,
+            total_ytd_anio_anterior_iva=total_ytd_anio_anterior_iva,
+            total_ytd_anio_actual_iva=total_ytd_anio_actual_iva,
+            total_variacion_ytd_porcentaje=total_variacion_ytd_porcentaje,
+            total_variacion_ytd_efectivo=total_variacion_ytd_efectivo,
+            total_existencia_tiendas=total_existencia_tiendas,
+            total_mos_tiendas=total_mos_tiendas,
+
+            total_mos_bodegas = total_mos_bodegas,
+            total_mos_almacenes = total_mos_almacenes
+
+        )
+        
+        return reporte_final_cierre_mes
+    
+
+    # Funcion para obtener el reporte de cierre de mes de 
+    def get_month_end_report_unode50(self, nombre_marca:str, mes:str):
+        """Metodo para obtener los datos de cierre de mes de Tous Hetal Mevy y Ricodeli"""
+        # print(f'mes: {mes}')
+        # Obtener registros de las tiendas por marca y transformalo en df
+        tiendas = [t.to_dict() for t in self.shared_service.get_all_stores_by_brand_name(nombre_marca)]
+        tiendas_df = pd.DataFrame(tiendas)
+
+        # Obtener los inventarios de todas las tiendas
+        inventarios_tiendas = self.inventario_service.get_store_inventories_total_stock_by_brand_name(nombre_marca)
+        inventarios_tiendas_dict = self.inventario_service.transform_store_inventories_total_stock(inventarios_tiendas)
+        inventarios_tiendas_df = pd.DataFrame(inventarios_tiendas_dict)
+        
+        # Obtener la suma de stock de los almacenes virtuales
+        inventarios_almacenes_virtuales = self.inventario_service.get_virtual_warehouses_inventories_total_stock_by_brand_name(nombre_marca)
+        suma_stock_almacenes_virtuales = inventarios_almacenes_virtuales[0]
+
+        # Obtener la suma de los inventarios fisicos de los almacenes
+        inventarios_almacenes_fisicos = self.inventario_service.get_physical_warehouses_inventories_total_stock_by_brand_name(nombre_marca)
+        # print(inventarios_almacenes_fisicos, len(inventarios_almacenes_fisicos))
+        # En caso de no tener existencia en los almacenes físicos (dos almacenes), retornar el diccionario con valores vacíos
+        if len(inventarios_almacenes_fisicos) == 0:
+            inventarios_almacenes_fisicos = [
+                ('7','CDMX01', 0)
+            ]
+
+        # Convertir los inventarios a un DataFrame
+        inventarios_almacenes_fisicos_df = pd.DataFrame(
+            inventarios_almacenes_fisicos,
+            columns=['id_almacen','whscode', 'total_existencias']  # Nombres de las columnas
+        )
+
+
+
+        # Obtener todas las ventas de inicio a fin del mes seleccionado
+        anio_actual = date.today().year
+        anio_anterior = date.today().year-1
+
+        meses_dict = {
+                'enero': 1, 'febrero': 2, 'marzo': 3, 'abril': 4,
+                'mayo': 5, 'junio': 6, 'julio': 7, 'agosto': 8,
+                'septiembre': 9, 'octubre': 10, 'noviembre': 11, 'diciembre': 12
+            }
+        
+        mes = mes.lower()
+        mes_numero = meses_dict[mes]
+        _,ultimo_dia_mes_anio_actual = calendar.monthrange(anio_actual,mes_numero)
+        _,ultimo_dia_mes_anio_anterior = calendar.monthrange(anio_anterior,mes_numero)
+
+        # Definimos las variables para el fin de mes del año actual y el anterior
+        fecha_inicio_mes_anio_actual = date(anio_actual,mes_numero,1)
+        fecha_fin_mes_anio_actual = date(anio_actual,mes_numero,ultimo_dia_mes_anio_actual)
+
+        fecha_fin_mes_anio_anterior = date(anio_anterior,mes_numero,ultimo_dia_mes_anio_anterior)
+        fecha_inicio_mes_anio_anterior = date(anio_anterior,mes_numero,1)
+        
+        # Obtener las ventas solamente del mes pero en el año actual de inicio a fin
+        ventas_mes_anio_actual= self.venta_service.get_sales_grouped_by_month_and_whscode(nombre_marca, fecha_inicio_mes_anio_actual, fecha_fin_mes_anio_actual)
+        ventas_mes_dict_anio_actual = self.venta_service.transform_sales_gropued_by_month(ventas_mes_anio_actual)
+        ventas_mes_actual_df = pd.DataFrame(ventas_mes_dict_anio_actual)
+
+
+        # Obtener las ventas solamente del mes pero del año anterior
+        ventas_mes_anio_anterior= self.venta_service.get_sales_grouped_by_month_and_whscode(nombre_marca, fecha_inicio_mes_anio_anterior, fecha_fin_mes_anio_anterior)
+        ventas_mes_dict_anio_anterior = self.venta_service.transform_sales_gropued_by_month(ventas_mes_anio_anterior)
+        ventas_mes_anterior_df = pd.DataFrame(ventas_mes_dict_anio_anterior)
+
+        #* Calculos del YTD
+        # Obtener todas las ventas de inicio de año hasta el fin del mes seleccionado
+        fecha_inicio_anio_actual = date(anio_actual,1,1)
+
+        fecha_inicio_anio_anterior = date(anio_anterior,1,1)
+
+        # Obtenemos el ytd del año anterior
+        ventas_ytd_anio_anterior = self.venta_service.get_sales_grouped_by_year_and_whscode(nombre_marca,fecha_inicio_anio_anterior, fecha_fin_mes_anio_anterior)
+        ventas_ytd_dict_anio_anterior = self.venta_service.transform_sales_gropued_by_year(ventas_ytd_anio_anterior)
+        ventas_ytd_anio_anterior_df = pd.DataFrame(ventas_ytd_dict_anio_anterior)
+        
+        # Obtenemos el ytd del año actual
+        ventas_ytd_anio_actual = self.venta_service.get_sales_grouped_by_year_and_whscode(nombre_marca,fecha_inicio_anio_actual, fecha_fin_mes_anio_actual)
+        ventas_ytd_dict_anio_actual = self.venta_service.transform_sales_gropued_by_year(ventas_ytd_anio_actual)
+        ventas_ytd_anio_actual = pd.DataFrame(ventas_ytd_dict_anio_actual)
+
+        # Obtenemos las ventas promedio de cada tienda
+        ventas_promedio_df = self.calcularVentaPromedio(nombre_marca,fecha_fin_mes_anio_actual)
+
+
+        reporte_cierre_mes = self.fusionar_dataframes_cierre_mes_unode50(ventas_mes_anterior_df,ventas_mes_actual_df,
+                                                                        ventas_ytd_anio_anterior_df,ventas_ytd_anio_actual,
+                                                                        tiendas_df,inventarios_tiendas_df, ventas_promedio_df,
+                                                                        suma_stock_almacenes_virtuales, inventarios_almacenes_fisicos_df)
+
+
+
+        return reporte_cierre_mes
+    
+
+    # Funcion para fusionar el reporte de cierre de mes de tumi
+    def fusionar_dataframes_cierre_mes_unode50(self, ventas_mes_anterior_df:pd.DataFrame, ventas_mes_actual_df:pd.DataFrame,
+                                                ventas_ytd_anio_anterior_df:pd.DataFrame,ventas_ytd_anio_actual:pd.DataFrame,
+                                                tiendas_df:pd.DataFrame, inventarios_tiendas_df:pd.DataFrame, ventas_promedio_df: pd.DataFrame,
+                                                suma_stock_almacenes_virtuales: float, inventarios_almacenes_fisicos_df:pd.DataFrame ):
+        
+        #Renombrar columnas de cada df
+        ventas_mes_anterior_df.rename(columns={"total_venta_neta_con_iva": "venta_mensual_anio_anterior_iva"}, inplace=True)
+        ventas_mes_actual_df.rename(columns={"total_venta_neta_con_iva": "venta_mensual_anio_actual_iva"}, inplace=True)
+        ventas_ytd_anio_anterior_df.rename(columns={"total_venta_neta_con_iva": "ytd_anio_anterior_iva"}, inplace=True)
+        ventas_ytd_anio_actual.rename(columns={"total_venta_neta_con_iva": "ytd_anio_actual_iva"}, inplace=True)
+
+        # Eliminar columnas insecesrias
+        ventas_mes_anterior_df.drop(columns=['mes'], inplace=True)
+        ventas_mes_actual_df.drop(columns=['mes'], inplace=True)
+        ventas_ytd_anio_anterior_df.drop(columns=['anio'], inplace=True)
+        ventas_ytd_anio_actual.drop(columns=['anio'], inplace=True)
+        # Fusionar mes con mes
+        ventas_mes_df = pd.merge(ventas_mes_anterior_df,ventas_mes_actual_df, on='whscode', how='outer')
+
+        # Fusionar año con año
+        ventas_ytd_df = pd.merge(ventas_ytd_anio_anterior_df,ventas_ytd_anio_actual, on='whscode', how='outer')
+
+        # Fusionar meses con el ytd
+        ventas_cierre_mes_df = pd.merge(ventas_mes_df,ventas_ytd_df, on='whscode', how='outer')
+
+        # Calcular las variaciones
+        ventas_cierre_mes_df = ventas_cierre_mes_df.fillna(0)
+        
+        ventas_cierre_mes_df['variacion_mes_porcentaje'] = np.where(ventas_cierre_mes_df['venta_mensual_anio_anterior_iva'] != 0,
+                                                        (((ventas_cierre_mes_df['venta_mensual_anio_actual_iva'] / ventas_cierre_mes_df['venta_mensual_anio_anterior_iva']) - 1)*100),0)
+        
+        ventas_cierre_mes_df['variacion_mes_efectivo'] =  ventas_cierre_mes_df['venta_mensual_anio_actual_iva'] - ventas_cierre_mes_df['venta_mensual_anio_anterior_iva']
+
+        ventas_cierre_mes_df['variacion_ytd_porcentaje'] = np.where(ventas_cierre_mes_df['ytd_anio_anterior_iva'] != 0,
+                                                        (((ventas_cierre_mes_df['ytd_anio_actual_iva'] / ventas_cierre_mes_df['ytd_anio_anterior_iva']) - 1)*100),0)
+        
+        ventas_cierre_mes_df['variacion_ytd_efectivo']  =   ventas_cierre_mes_df['ytd_anio_actual_iva'] - ventas_cierre_mes_df['ytd_anio_anterior_iva']
+
+
+        # Hacer el merge con los inventarios
+        ventas_cierre_mes_df = pd.merge(ventas_cierre_mes_df,inventarios_tiendas_df, on='whscode', how='outer')
+
+        # Hacemos el merge con las ventas promedio
+        ventas_cierre_mes_df = pd.merge(ventas_cierre_mes_df,ventas_promedio_df, on='whscode', how='outer')
+
+        # Calculamos el mos
+        ventas_cierre_mes_df['mos'] = ventas_cierre_mes_df['existencia'] / ventas_cierre_mes_df['venta_promedio'].astype(float)
+
+        ventas_cierre_mes_df = pd.merge(ventas_cierre_mes_df,tiendas_df[['whscode','nombre_sucursal','tipo_tienda','ciudad','estado_operativo','comparabilidad']],on='whscode',how='left')
+
+        # Llenamos con 0 las existencias que no tengan coincidencia
+        ventas_cierre_mes_df= ventas_cierre_mes_df.fillna(0)
+
+
+        # Separar las tiendas que no son cancun ni cdmx
+        tiendas_sin_cdmx = ventas_cierre_mes_df[~ventas_cierre_mes_df['whscode'].str.contains('MX', case=False, na=False)].copy()
+
+        # Separamos en un df las tiendas de cancun para calcular sus porcentajes de participacion
+
+        almacen_fisico_cdmx = inventarios_almacenes_fisicos_df[inventarios_almacenes_fisicos_df['whscode']=='CDMX01'].copy() 
+
+        # Filtrar filas donde 'whscode' contenga "MEX"
+        tiendas_vp_cdmx_df = ventas_cierre_mes_df[ventas_cierre_mes_df['whscode'].str.contains('MX', case=False, na=False)].copy()
+
+        #* Separamos en un df las tiendas de cdmx para calcular sus porcentajes de participacion
+        # Realizar calculo de porcentaje de participacion en tiendas 
+
+        porcentaje_participacion_venta_cdmx = self.calcularPorcentajeDeParticipacionDeVenta(tiendas_vp_cdmx_df)
+        
+        # Realizar la reparticion de almacen entre las tiendas
+
+        tiendas_cdmx_con_piezas_almacen = self.calcularPiezasAlmacenFisico(porcentaje_participacion_venta_cdmx, almacen_fisico_cdmx)
+
+        # Realizar el calculo del mos_almacen de tiendas con almacen fisico
+        tiendas_cdmx_df = self.calcularMosAlmacen(tiendas_cdmx_con_piezas_almacen)
+
+        #* Realizar la fuision entre los tres dfs, el de tijuana, con cancun y cdmx ya con sus almacenes de cun y cdmx
+        ventas_cierre_mes_df = pd.concat([tiendas_sin_cdmx,tiendas_cdmx_df], ignore_index=True)
         # Llenamos con 0 los almacenes que no tengan existencia
         ventas_cierre_mes_df = ventas_cierre_mes_df.fillna(0)
 
